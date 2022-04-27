6bf83d7d455f47d786ee80164613ea08e4f52d8f
MostlyMaple <81451581+MostlyMaple@users.noreply.github.com>
2022-04-27 13:25:18 -0500

    Update cartActions.js
    get-item route doesn't work in golang - switched back to django

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -10,8 +10,8 @@ import {
 
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-    //const {data} = await axios.get(`http://35.224.232.15/api/get-item/${id}/`)
-    const { data } = await axios.get(`http://147.182.178.230:4000/v1/get-items/?id=${id}`)
+    const {data} = await axios.get(`http://35.224.232.15/api/get-item/${id}/`)
+    //const { data } = await axios.get(`http://147.182.178.230:4000/v1/get-items/?id=${id}`)
     dispatch({
         type:CART_ADD_ITEM,
         payload:{
